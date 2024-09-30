043734bb1e85bb845612afafd1603cbfe9057124
“Riwaj” <shresthariwaj10@gmail.com>
2024-09-30 13:59:30 +0545

    landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/index.html"],
+  content: ["./build/*.html"],
   theme: {
     extend: {},
   },
   plugins: [],
+  darkMode:"class",
 }
 
